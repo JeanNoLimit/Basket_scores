030bbfb5fbba333cf7c281654400bf4b0ebf9896
Jean-Noël richert <jean.noel.richert@gmail.com>
2024-02-07 15:24:41 +0100

    récupération journée selectionnée

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState ,useEffect } from 'react'
+import { useState ,useEffect, useRef } from 'react'
 import '../styles/App.css'
 import {Select} from './form/Select.jsx'
 
@@ -6,26 +6,30 @@ import {Select} from './form/Select.jsx'
 function App() {
 
   // On va récupérer le résultat de l'API dans cette variable.
-  const [teams, setTeams] = useState([]);
-  // Va nous servir à vérifier si la liste des équipes à déjà été chargé, pour ne pas appeler l'API à chaque chargement du composant
-  const [isMounted, setIsMounted] = useState(false);
-  const [nbJournee, setNbJournee] = useState(0);
+  const [teams, setTeams] = useState({});
+  // Va nous servir à vérifier si la liste des équipes à déjà été chargé, pour ne pas appeler l'API à chaque chargement du composant.
+  // On utilise useRef car cette variable n'est pas lié à un element à render. On évite ainsi un re-render après modification.
+  const isMounted = useRef(false);
+  const [nbJournee, setNbJournee] = useState(45);
+  const [numJournee, setNumJournee] = useState(1);
 
-
+  // console.log(numJournee)
   /**
    * Appel de l'API sportdb pour récupérer la liste des équipes du championnat.
    * Cela va nous permettre de connaitre le nombre de journée par saison et l'affichage du détail des équipes par la suite...
    */
   useEffect(() => {   
-    async function fetchTeams() {
-      if(!isMounted) {
-        try {
-          const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=French%20LNB`);
+    if(isMounted.current == false) {
+      async function fetchTeams() {
+
+        try { 
+          const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=French%20LNB`)
           await response.json().then((json) => {
             setTeams(json); 
             setNbJournee((json.teams.length*2)-2);
-            setIsMounted(true);
+            isMounted.current = true;
           } );
+          
         }  
         catch (error) {
           console.log(error);
@@ -34,17 +38,18 @@ function App() {
          // sera utile plus tard, lorsqu'une barre de chargement sera visible sur le site.
         }
       }
+      fetchTeams();
     }
-    fetchTeams();
+    
   }, []);
 
   return (
     <>
-      <SearchBar nbJournee={nbJournee} />
+      <SearchBar nbJournee={nbJournee} onChange={setNumJournee}/>
+      <h2 className="">Vous avez choisi d'afficher la journée : {numJournee } </h2>
     </>
   )
 }
-
 
 /**
  * formulaire de recherche des matchs par journée.
@@ -52,11 +57,14 @@ function App() {
  * @param {int} nbJournee 
  * @returns {JSX.Element} 
  */
-function SearchBar({nbJournee}) {
+function SearchBar({nbJournee, onChange }) {
   
   return <div>
     <div className="mb-3">
-      <Select nbJournee = {nbJournee}/>
+      <Select 
+        nbJournee = {nbJournee}
+        onChange={onChange} 
+      />
     </div>
   </div>
 }
--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -1,19 +1,20 @@
-export function Select({nbJournee}) {
-
-    // const [data, setData] = useState(undefined);
+export function Select({nbJournee, onChange }) {
     
     const getOptions = () => {
         let content = [];
         for (let i=1; i<=nbJournee; i++) {
-            content.push(<option key={i}>Journée {i}</option>)
+            content.push(<option key={i} value={i}  >Journée {i}</option>)
         }
         return content;
     }
 
     return <div>
-        <select className="form-select">
+        <select 
+            className="form-select" 
+            onChange={(e)=> onChange(e.target.value)}
+        >
             <option defaultValue> Choisir une journée à afficher...</option>
-           {getOptions()}
+            {getOptions()}
         </select>
     </div>
 }
