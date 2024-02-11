906c2f1f580714013b34940305e07b52b9c1ca76
Jean-Noël richert <jean.noel.richert@gmail.com>
2024-02-11 18:10:21 +0100

    affichage lieu du match

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,15 +6,16 @@ export function Card({event, teams}) {
     
     console.log("les équipes dans card...:")
     console.log(teams)
-    const logoDomicile = teams.length>0 ? teams.find(team => team.idTeam == event.idHomeTeam).strTeamBadge : "noLogo"
-    const logoVisiteur = teams.length>0 ? teams.find(team => team.idTeam == event.idAwayTeam).strTeamBadge : "noLogo"
+    const homeTeam = teams.length>0 ? teams.find(team => team.idTeam == event.idHomeTeam) : "noLogo"
+    const awayteam = teams.length>0 ? teams.find(team => team.idTeam == event.idAwayTeam) : "noLogo"
     
+    console.log(homeTeam.strStadiumLocation);
     return <>
         <div className="card shadow border-0 rounded-5 score-card h-100" >
             <div className="p-3 ">
                 <LeagueInformation numJournee={event.intRound} date={event.dateEvent}/>
                 <div className="row align-items-center row-score">
-                    <Team teamName={event.strHomeTeam} logo={logoDomicile} key={event.strHomeTeam}/>
+                    <Team teamName={event.strHomeTeam} logo={homeTeam.strTeamBadge} key={event.strHomeTeam}/>
                     <div className="col d-flex flex-column justify-content-between align-items-center score-container">
                         <div className="score-display">
                             <span className="fw-medium">{event.intHomeScore}</span>
@@ -23,13 +24,12 @@ export function Card({event, teams}) {
                         </div>
                         <button type="button" className="btn btn-outline-dark rounded-pill border border-dark border-1 fw-medium btn-sm w-100">Détails</button>
                     </div>
-                    <Team teamName={event.strAwayTeam} logo={logoVisiteur} key={event.strAwayTeam}/>
+                    <Team teamName={event.strAwayTeam} logo={awayteam.strTeamBadge} key={event.strAwayTeam}/>
                 </div>
             </div> 
-            <div className="w-100 rounded-bottom-5 d-flex align-items-center justify-content-center location-wrapper">
-                {/* <!-- Penser à modifier icon fontawsome pour React --> */}
+            <div className="w-100 rounded-bottom-5 d-flex align-items-center justify-content-center location-wrapper px-3">
                 <div className="d-flex flex-row align-items-baseline text-white fw-medium">
-                    <i className="fa-solid fa-location-dot"></i><p className="ps-2 m-0">Stade de Ada Blois</p>
+                    <i className="fa-solid fa-location-dot"></i><p className="ps-2 m-0">{homeTeam.strStadium}, {teams.length>0 ? (homeTeam.strStadiumLocation).replace(", France", "") : "...chargement en cours..."}</p>
                 </div>
             </div>
         </div>
