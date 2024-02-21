f7dc84cb930b82b770ce1e613147abea89ef8d75
Jean-Noël richert <jean.noel.richert@gmail.com>
2024-02-21 11:57:05 +0100

    modification navbar, ajout logo test

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,16 +3,16 @@ import {Outlet, Link} from 'react-router-dom'
 export default function NavBar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-dark-subtle">
+            <nav className="navbar navbar-expand-lg bg-white fw-medium fs-5 text-dark text-uppercase">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to={`/`}>BasketScores</Link>
+                    <Link className="navbar-brand" to={`/`}><img className="mx-3" src="/img/logo/logo_basket_score.png" alt="logo basket scores" height="55" /></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to={`/`}>Accueil</Link>
+                                <Link className="nav-link" aria-current="page" to={`/`}>Accueil</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to={`teams`}>Les équipes</Link>
