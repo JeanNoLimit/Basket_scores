f6e5db02c737c25f40a7ccfb5d75ab1632f52a26
Jean-Noël richert <jean.noel.richert@gmail.com>
2024-02-26 23:58:32 +0100

    affichage liste des équipes

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,29 +1,47 @@
-import { Outlet } from "react-router-dom"
+import { 
+    Outlet,
+    useOutletContext,
+    NavLink
+} from "react-router-dom"
+import '../styles/teams.css'
 
 export default function Teams() {
     
+    const teams = useOutletContext();
+    console.log(teams);
+
     return (
        <>
-       <div style={{width:280}}>
-        <button class="btn btn-primary d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">Toggle offcanvas</button>
+       <div className="d-flex flex-row">
+            <div style={{width:280,minHeight:100 +'vh'}}>
+                <button className="btn btn-light d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">filtre</button>
 
-        <div class="offcanvas-lg offcanvas-end" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasResponsiveLabel">Responsive offcanvas</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
+                <div className="offcanvas-lg offcanvas-start" tabIndex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title text-uppercase" id="offcanvasResponsiveLabel">Les équipes</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body " style={{minHeight:100 +'vh'}}>
+                        <div className="d-flex flex-column flex-shrink-0 p-3 bg-light m-0">
+                            <ul className="nav nav-pills flex-column mb-auto">
+                                {teams.map((team) =>
+                                <li className="nav-item" key={team.idTeam}>
+                                    <NavLink className="nav-link link-dark" aria-current="page" to={`/teams/${team.idTeam}`}>
+                                        <img src={team.strTeamBadge} alt="" style={{height: 2 + "rem"}}/>
+                                        <span className="ps-2">{team.strTeam}</span>
+                                    </NavLink>
+                                </li>
+                                
+                                )}
+                            
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="offcanvas-body">
-                <ul className="">
-                    <li>
-                        equipe1
-                    </li>
-                    <li> equipe2</li>
-                </ul>
-            </div>
-        </div>
-
-       </div>
         <Outlet />
+       </div>
+       
        </>
     )
 }
